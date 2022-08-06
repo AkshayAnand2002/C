bd21f62b83b3372c5decba56fe7dfa7145bf24cd
AkshayAnand2002 <93818352+AkshayAnand2002@users.noreply.github.com>
2022-08-06 08:35:08 +0530

    Create Tech_Mahindra_Find_No_Of_Curtains_Of_Required_Length.c

---
+++ Tech_Mahindra_Find_No_Of_Curtains_Of_Required_Length.c
@@ -0,0 +1,46 @@
+/*
+A Cloth merchant has some pieces of cloth of different lengths. 
+He has an order of curtains of length of 12 feet. 
+He has to find how many curtains can  be made from these pieces. 
+Length of pieces of cloth is recorded in feet.
+Note : You are expected to write code in the findTotalCurtains function only 
+which receive the first parameter as the number of items in the array and 
+second parameter as the array itself. You are not required to take the input from the console.
+Example
+Finding the total curtains from a list of 5 cloth pieces.
+Input
+input 1 : 5
+input 2 : 3 42 60 6 14
+Output
+9
+Explanation
+The first parameter 5 is the size of the array. 
+Next is an array of measurements in feet. 
+The total number of curtains is 5 which is calculated as under
+3 -> 0
+42 -> 3
+60 -> 5
+6 -> 0
+14 -> 1
+total = 9
+*/
+#include<stdio.h>
+int findTotalCurtains(int n,int arr[]){
+    int feet,total=0;
+    for(int i=0;i<n;i++){
+        feet=arr[i]/12;
+        total=total+feet;
+    }
+    return total;
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int array[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&array[i]);
+    }
+    int result=findTotalCurtains(n,array);
+    printf("%d",result);
+    return 0;
+}
